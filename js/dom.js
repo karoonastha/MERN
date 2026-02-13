256596f672612181263087fe16457f002ae1790e
sagun basnet <basnetsagun8@gmail.com>
2026-02-13 13:38:32 +0545

    dom2

--- js/dom.js
+++ js/dom.js
@@ -13,16 +13,52 @@
 
 // const btn2 = document.getElementsByTagName("button");
 
-const btn2 = document.querySelector(".reset");
-const btn3 = document.querySelector("button");
-const div = document.querySelector("div");
+// const btn2 = document.querySelector(".reset");
+// const btn3 = document.querySelector("button");
+// const div = document.querySelector("div");
 
-btn3.addEventListener("click", () => {
-  //   div.style.backgroundColor = "black";
-  div.style.display = "none";
-});
+// const red = document.querySelector(".red");
 
-btn2.addEventListener("click", () => {
-  //   div.style.backgroundColor = "white";
-  div.style.display = "block";
+// const color = document.querySelectorAll(".color");
+// const body = document.querySelector("body");
+
+// for (let i = 0; i < color.length; i++) {
+//   color[i].addEventListener("click", () => {
+//     body.style.backgroundColor = color[i].innerHTML.toLowerCase();
+//   });
+// }
+
+div.innerHTML = "Hello World";
+// btn3.addEventListener("click", () => {
+//   //   div.style.backgroundColor = "black";
+//   div.style.display = "none";
+// });
+
+// btn2.addEventListener("click", () => {
+//   //   div.style.backgroundColor = "white";
+//   div.style.display = "block";
+// });
+
+// const changeColor = (color) => {
+//   div.style.backgroundColor = color;
+// };
+
+const sidebar = document.querySelector(".sidebar");
+// const btn = document.querySelector(".icon");
+const cross = document.querySelector(".cross");
+
+const bar = document.querySelector(".bar");
+bar.addEventListener("click", () => {
+  // sidebar.style.marginLeft = "-8rem";
+  // sidebar.classList.add("toggle");
+  sidebar.classList.toggle("toggle");
+  // cross.style.display = "none";
+  // bar.style.display = "block";
 });
+// bar.addEventListener("click", () => {
+//   // sidebar.style.marginLeft = "-8rem";
+//   // sidebar.classList.add("toggle");
+//   sidebar.classList.remove("toggle");
+//   cross.style.display = "block";
+//   bar.style.display = "none";
+// });
