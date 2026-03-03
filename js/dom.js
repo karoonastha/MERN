b9dfe06c2b3c3dc843b0c642ed4af0ba2732e5f6
Karuna Shrestha <karoonasth@gmail.com>
2026-03-03 20:28:04 +0545

    Button

--- js/dom.js
+++ js/dom.js
@@ -1,64 +1,32 @@
-// function handleClick(){
+// const btn1 = document.querySelector("#greenbtn");
+// const btn2 = document.querySelector("#bluebtn");
+// const btn3 = document.querySelector("#redbtn");
+// const btn4 = document.querySelector("#yellowbtn");
+// const body = document.querySelector("body")
 
-// }
-
-// const handleClick = (a,b) => {
-//   alert("Button clicked");
-// };
-
-// handleClick(1, ()=> console.log("hello"))
-
-// const btn = document.getElementsByClassName("clickbtn");
-// const btn1 = document.getElementById("btn");
-
-// const btn2 = document.getElementsByTagName("button");
-
-// const btn2 = document.querySelector(".reset");
-// const btn3 = document.querySelector("button");
-// const div = document.querySelector("div");
-
-// const red = document.querySelector(".red");
-
-// const color = document.querySelectorAll(".color");
-// const body = document.querySelector("body");
-
-// for (let i = 0; i < color.length; i++) {
-//   color[i].addEventListener("click", () => {
-//     body.style.backgroundColor = color[i].innerHTML.toLowerCase();
-//   });
-// }
-
-div.innerHTML = "Hello World";
-// btn3.addEventListener("click", () => {
-//   //   div.style.backgroundColor = "black";
-//   div.style.display = "none";
+// btn1.addEventListener ("click", () => {
+//     alert("BackgroundColor is green.");
+//   body.style.backgroundColor = "green";
 // });
 
-// btn2.addEventListener("click", () => {
-//   //   div.style.backgroundColor = "white";
-//   div.style.display = "block";
+// btn2.addEventListener ("click", () => {
+//     alert("BackgroundColor is blue.");
+//   body.style.backgroundColor = "blue";
 // });
 
-// const changeColor = (color) => {
-//   div.style.backgroundColor = color;
-// };
-
-const sidebar = document.querySelector(".sidebar");
-// const btn = document.querySelector(".icon");
-const cross = document.querySelector(".cross");
-
-const bar = document.querySelector(".bar");
-bar.addEventListener("click", () => {
-  // sidebar.style.marginLeft = "-8rem";
-  // sidebar.classList.add("toggle");
-  sidebar.classList.toggle("toggle");
-  // cross.style.display = "none";
-  // bar.style.display = "block";
-});
-// bar.addEventListener("click", () => {
-//   // sidebar.style.marginLeft = "-8rem";
-//   // sidebar.classList.add("toggle");
-//   sidebar.classList.remove("toggle");
-//   cross.style.display = "block";
-//   bar.style.display = "none";
+// btn3.addEventListener ("click", () => {
+//     alert("BackgroundColor is red.");
+//   body.style.backgroundColor = "red";
 // });
+
+// btn4.addEventListener ("click", () => {
+//     alert("BackgroundColor is yellow.");
+//   body.style.backgroundColor = "yellow";
+// });
+
+
+ const body = document.querySelector("body")
+
+ const changeColor = (color) => {
+    body.style.backgroundColor = color;
+ }
